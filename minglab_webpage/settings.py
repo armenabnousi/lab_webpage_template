25b6bd59b6fd6cb976aea327e824e8b67d6530e2
armenabnousi <a.abnousi@outlook.com>
2019-03-25 09:47:37 -0400

    added static_root to settings

--- minglab_webpage/settings.py
+++ minglab_webpage/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/data/')
 STATIC_URL = '/static/'
